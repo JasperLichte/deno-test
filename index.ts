e99df3c97e9f924d36aa8530266ef7d27afa533f
jasper.lichte <jasper.lichte@check24.de>
2021-05-03 11:38:46 +0200

    [master] Hello world

--- index.ts
+++ index.ts
@@ -1,6 +1,17 @@
-addEventListener("fetch", (event) => {
-  const response = new Response("Hello World!", {
-    headers: { "content-type": "text/plain" },
-  });
-  event.respondWith(response);
+import { Application, Router, RouterContext } from "https://deno.land/x/oak@v6.1.0/mod.ts";
+
+const PORT = 1337;
+
+const app = new Application();
+
+const router = new Router();
+
+router.get("/test", (context: RouterContext) => {
+  context.response.status = 200;
+  context.response.body = Math.random();
 });
+
+app.use(router.routes());
+
+console.log(`Listening on port ${PORT}...`);
+await app.listen(`localhost:${PORT}`);
